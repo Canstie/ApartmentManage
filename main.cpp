57500173e5ce7b499e7a608753630902cc7e796c
Canstie <3143800341@qq.com>
2024-07-14 14:26:05 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,18 +8,6 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     Login login;
-
-    QObject::connect(&login, &Login::loginSuccess, [&](const QString &account, bool isAdmin) {
-        if (isAdmin) {
-            MainWindow *mainwindow = new MainWindow;
-            mainwindow->show();
-        } else {
-            userui *user1 = new userui;
-            user1->show();
-        }
-        login.close();
-    });
-
     login.show();
     return a.exec();
 }
